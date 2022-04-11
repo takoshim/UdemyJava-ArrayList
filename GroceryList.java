c9e51d06b7793345d518728961330f95e47ccf35
tako2039 <takeo2039@gmail.com>
2022-04-11 16:43:34 -0500

    add function findGroceryItem

--- GroceryList.java
+++ GroceryList.java
@@ -5,15 +5,15 @@ import java.util.ArrayList;
 public class GroceryList {
     private ArrayList<String> groceryList = new ArrayList<String>();
 
-    public void addGroceryItem(String item) {
-        groceryList.add(item);
-    }
-
     public void printGroceryList() {
         System.out.println("You have " + groceryList.size() + " items in your grocery list.q");
         for (int i=0; i<groceryList.size(); i++) {
             System.out.println( (i+1) + ". " + groceryList.get(i));
         }
+    }
+
+    public void addGroceryItem(String item) {
+        groceryList.add(item);
     }
 
     public void modifyGroceryList(int position, String newItem) {
@@ -24,5 +24,14 @@ public class GroceryList {
     public void removeGroceryItem(int position) {
         String theItem = groceryList.get(position);
         groceryList.remove(position);
+    }
+
+    public String findGroceryItem(String searchItem) {
+        // boolean exists = groceryList.contains(searchItem);
+        int position = groceryList.indexOf(searchItem);
+        if (position >=0) {
+            return groceryList.get(position);
+        }
+        return null;
     }
 }
